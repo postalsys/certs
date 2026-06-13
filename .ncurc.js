90788b881a1d47ecaed2083af3f07436a44cbe09
Andris Reinman <andris@reinman.eu>
2026-06-13 11:51:45 +0300

    fix: bumped deps

--- .ncurc.js
+++ .ncurc.js
@@ -1,4 +1,6 @@
 module.exports = {
     upgrade: true,
-    reject: ['eslint-config-prettier', 'express']
+    reject: ['eslint-config-prettier', 'express'],
+    // do not update joi from 17, check emailengine joi compatibility first
+    target: (name) => (name === 'joi' ? 'minor' : 'latest')
 };
